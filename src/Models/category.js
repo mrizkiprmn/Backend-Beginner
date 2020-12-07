f44533f52921208930a031870987cc822c6c4485
mrizkiprmn <64014794+mrizkiprmn@users.noreply.github.com>
2020-12-08 01:45:34 +0700

    update category

--- src/Models/category.js
+++ src/Models/category.js
@@ -1,58 +1,58 @@
-const db = require("../Configs/db");
-require("../Controllers/category");
-const category = {};
+const db = require('../Configs/db');
+const categories = {};
 
-category.get = () => {
-    return new Promise((resolve, reject) => {
-        db.query("SELECT * FROM public.category ORDER BY idcategory ASC")
+
+categories.get = () =>{
+    return new Promise((resolve, reject) =>{
+        db.query("SELECT * FROM public.category ORDER BY id ASC")
         .then((res) => {
-         if (res.rows.length == 0) {
-             resolve({msg: "Data is Empty"});
-         } else {
-             resolve(res.rows);
-         }
+            if (res.rows.length == 0) {
+                resolve("Categories is empty!");
+            } else {
+                resolve(res.rows);
+            };
         })
         .catch((err) => {
             reject(err);
+        });
+    });
+};
+
+categories.add = (data) =>{
+    return new Promise((resolve, reject) =>{
+        db.query(`INSERT INTO public.category(name) VALUES ('${data.name}')`)
+        .then((res) => {
+            resolve(data);
         })
-    })
-}
+        .catch((err) => {
+            reject("Input Categories!");
+        });
+    });
+};
 
-
-category.add = (data) => {
-    return new Promise((resolve, reject) => {
-        db.query(`INSERT INTO public.category(category) VALUES ('${data.category}')`)
+categories.update = (data) =>{
+    return new Promise((resolve, reject) =>{
+        db.query(`UPDATE public.category SET name='${data.name}' WHERE id=${data.id}`)
         .then((res) => {
             resolve(data);
         })
         .catch((err) => {
             reject(err);
-        })
-    })
-}
+        });
+    });
+};
 
-category.update = (data) => {
-    return new Promise((resolve, reject) => {
-        db.query(`UPDATE public.category SET category='${data.category}' WHERE idcategory=${data.idcategory}`)
+
+categories.del = (id) =>{
+    return new Promise((resolve, reject) =>{
+        db.query(`DELETE FROM public.category WHERE id=${id}`)
         .then((res) => {
-            resolve(data);
+            resolve(id);
         })
         .catch((err) => {
             reject(err);
-        })
-    })
-}
+        });
+    });
+};
 
-category.del = (data) => {
-    return new Promise((resolve, reject) => {
-        db.query(`DELETE FROM public.category WHERE idcategory=${data}`)
-        .then((res) => {
-            resolve(data);
-        })
-        .catch((err) => {
-            reject(err);
-        })
-    })
-}
-
-module.exports = category;
+module.exports = categories;
