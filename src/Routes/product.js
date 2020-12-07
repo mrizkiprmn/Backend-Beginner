10858e97674e4a2ee8e2e744322e7d9b2d899eaf
mrizkiprmn <64014794+mrizkiprmn@users.noreply.github.com>
2020-12-08 01:39:48 +0700

    update endpoint product

--- src/Routes/product.js
+++ src/Routes/product.js
@@ -1,11 +1,13 @@
 const express = require("express");
+
 const routes = express.Router();
 const ctrl = require("../Controllers/product");
 
-routes.get("/", ctrl.get);
-routes.get("/:name", ctrl.getName);
+
+routes.get('/', ctrl.getAll);
+routes.get('/:id', ctrl.get);
 routes.post("/", ctrl.add);
 routes.put("/", ctrl.update);
-routes.delete("/:idfood", ctrl.del);
+routes.delete("/:id", ctrl.del);
 
 module.exports = routes;
