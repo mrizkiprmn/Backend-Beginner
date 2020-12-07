e0fe91f5dde47ba0d06484f1ba7c183f777b0fee
mrizkiprmn <64014794+mrizkiprmn@users.noreply.github.com>
2020-12-08 01:53:45 +0700

    add query params delete

--- src/Controllers/category.js
+++ src/Controllers/category.js
@@ -1,44 +1,44 @@
-const category = {};
-const model = require("../Models/category");
-const response = require("../Helpers/response");
+const categories = {};
+const model = require('../Models/category');
+const response = require('../Helpers/response');
 
+categories.get = async (req, res) => {
+    try{
+        const result = await model.get();
+        return response(res, 200, result);
+    }catch (error){
+        return response(res, 500, error);
 
-category.get = async (req, res) => {
-    try  {
-         const result = await model.get();
-         return response(res, 200, result);
-    } catch (error) {
-      return response(res, 500, error);
-    }
- }
- 
- 
- category.add = async (req, res) =>{
-      try {
-           const result = await model.add(req.body);
-           return response(res, 201, result);
-      } catch(error) {
-           return response(res, 500, error);
-      }
- }
- 
- category.update = async (req, res) => {
+    };
+  
+};
+
+categories.add = async (req, res) => {
     try {
-         const result = await model.update(req.body);
-         return response(res, 200, result);
-    } catch(error) {
-         return response(res, 500, error);
-    }
+        const result = await model.add(req.body);
+        return response(res, 201, result);
+    } catch (error){
+        return response(res, 500, error);
+    };
+    
+};
 
+categories.update = async (req, res) => {
+    try {
+        const result = await model.update(req.body);
+        return response(res, 200, result);
+    } catch (error){
+        return response(res, 400, error);
+    }
+};
+
+categories.del = async (req, res) => {
+   try {
+       const result = await model.del(req.params.id);
+        return response(res, 200, result);
+   }catch (error) {
+        return response(res, 400, error);
+   }
 }
 
- category.del = async (req, res) =>{
-      try {
-           const result = await model.del(req.params.idcategory);
-           return response(res, 200, result);
-      } catch (error) {
-           return response(res, 404, error);
-      }
- }
-
-module.exports = category;
+module.exports = categories;
