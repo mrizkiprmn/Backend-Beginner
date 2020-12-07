5c8264c3be120cde3ad5ce1c32b661175d4e1a1c
mrizkiprmn <64014794+mrizkiprmn@users.noreply.github.com>
2020-12-08 01:54:40 +0700

    add query params,sort & join

--- src/Controllers/product.js
+++ src/Controllers/product.js
@@ -1,56 +1,63 @@
 const product = {};
-const model = require("../Models/product");
-const response = require("../Helpers/response");
+const model = require('../Models/product');
+const response = require('../Helpers/response');
 
+product.getAll = async (req, res) => {
+    const { search } = req.query;
+    const { orderBy, sort } = req.query;
+    let result;
+    try {
+      if (search) {
+        result = await model.getSearch(search);
+      } else if (orderBy) {
+        result = await model.getSort(orderBy, sort);
+      } else {
+        result = await model.getAll();
+      };
+      return response(res, 200, result);
+    } catch (error) {
+      return response(res, 500, error);
+    };
+  };
 
 product.get = async (req, res) => {
-   try  {
-        const result = await model.get();
-        return response(res, 200, result);
-   } catch (error) {
-     return response(res, 500, error);
-   }
-}
-
-product.getName = async (req, res) =>{
-     try {
-          const result = await model.getName(req.params.name);
-          return response(res, 200, result);
-     } catch (error) {
-          return response(res, 401, error);
-     }
-}
+    try {
+      const result = await model.get(req.params.id);
+      return response(res, 200, result);
+    } catch (error) {
+      return response(res, 500, error);
+    };
+  };
 
 
-product.add = async (req, res) =>{
-     try {
-          const result = await model.add(req.body);
-          return response(res, 201, result);
-     } catch(error) {
-          return response(res, 500, error);
-     }
-}
-
+product.add = async (req, res) => {
+    try {
+        const result = await model.add(req.body);
+        return response(res, 201, result);
+    } catch (error){
+        return response(res, 400, error);
+    };
+    
+};
 
 product.update = async (req, res) => {
-     try {
-          const result = await model.update(req.body);
-          return response(res, 200, result);
-     } catch(error) {
-          return response(res, 500, error);
-     }
+    try {
+        const result = await model.update(req.body);
+        return response(res, 200, result);
+    } catch (error){
+        return response(res, 400, error);
+    };
+};
 
-}
-product.del = async (req, res) =>{
-     try {
-          const result = await model.del(req.params.idfood);
-         return response(res, 200, result);
-     } catch (error) {
-          return response(res, 404, error);
-     }
-}
-
-
+product.del = async (req, res) => {
+   try {
+       const result = await model.del(req.params.id);
+        return response(res, 200, result);
+   }catch (error) {
+        return response(res, 400, error);
+   };
+};
 
 
+ 
 module.exports = product;
