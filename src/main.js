8d07634dd915efe6c9fd7f40560b1665162080b4
mrizkiprmn <64014794+mrizkiprmn@users.noreply.github.com>
2020-12-12 01:23:23 +0700

    add routes history

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ const express = require("express");
 const routes = express.Router();
 const product = require("./Routes/product");
 const category = require("./Routes/category");
-
+const history = require("./Routes/history");
 
 routes.use("/product", product);
 routes.use("/category", category);
-
+routes.use("/history", history);
 
 module.exports = routes;
